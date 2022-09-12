c2947a6ca9cedaba3984b04643c4e9113520c5a9
atrox3d <atrox3d@gmail.com>
2022-09-12 17:33:56 +0200

    173. Styling the Webpage

--- examples/jp-example.py
+++ examples/jp-example.py
@@ -4,7 +4,13 @@ import justpy as jp
 @jp.SetRoute("/home")                                       # use decorator OR jp.Route()
 def home():                                                 # request handler
     wp = jp.WebPage()                                       # web page
-    jp.Div(a=wp, text='Hello World!')                       # html element attached to web page
+    jp.Div(                                                 # html element attached to web page
+        a=wp,
+        text='Hello World!',
+        classes="text-green-600 bg-yellow-500 "              # https://tailwindcss.com/docs
+                "font-serif text-lg"
+    )
+
     jp.Div(a=wp, text='Hello Again!')
     return wp                                               # must return page
 
