3f2359d30c41ed6af0e8d0e81ed154a6d477af3e
atrox3d <atrox3d@gmail.com>
2022-09-12 14:18:55 +0200

    170. Implementing the Definition Class

--- definition.py
+++ definition.py
@@ -1,6 +1,16 @@
+import pandas
+
+
 class Definition:
     def __init__(self, term):
         self.term = term
 
     def get(self):
-        pass
+        df = pandas.read_csv('data.csv')
+        definition = tuple(df.loc[df['word'] == self.term]['definition'])
+        return definition
+
+
+d = Definition(term='sun')
+print(d.get())
+
