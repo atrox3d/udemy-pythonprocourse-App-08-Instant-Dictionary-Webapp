a4e8446c20c0007a384b52ecacb1a21d3b0b4550
atrox3d <atrox3d@gmail.com>
2022-09-13 17:03:10 +0200

    185. Getting Output in the Output Div

--- webapp/dictionary.py
+++ webapp/dictionary.py
@@ -4,8 +4,16 @@ import justpy as jp
 class Dictionary:
     path = '/dictionary'
 
-    @classmethod
-    def serve(cls, request):
+    """
+    without class method serve is called as instance method but without the self, because it is registered in the
+    Route method as Dictionary.serve
+    So it is called without self but it doesnt rise an Exception because justpy passes a request parameter.
+    As a result we dont get any error but we have a wrong self variable.
+    To solve this we use the classmethod decorator, in this way when serve is called python passes the class
+    as 1st argument ad request as the second
+    """
+    @classmethod                                                                # called from Dictionary.serve, no self
+    def serve(cls, request):                                                    # so request doesnt go to 1st param
         wp = jp.QuasarPage(tailwind=True)                                       # enable tailwind css support
         div = jp.Div(a=wp, classes='bg-gray-200 h-screen')
         jp.Div(a=div, text='Instant English Dictionary', classes='text-4xl m-2')
@@ -15,8 +23,9 @@ class Dictionary:
                 text='Get the definition of any English word instantly as you type.',
                 classes='text-large'
         )
+        input_div = jp.Div(a=div, classes='grid grid-cols-2')
         jp.Input(
-                a=div, placeholder='Type in a word here...',
+                a=input_div, placeholder='Type in a word here...',
                 classes='m-2 '                                                  # margin 2
                         'bg-gray-100 '                                          # gray backgroud
                         'border-2 '                                             # border 2
@@ -29,14 +38,33 @@ class Dictionary:
                         'py-2 '                                                 # padding
                         'px-4 '
         )
-        jp.Div(
+        output_div = jp.Div(
                 a=div,
                 classes='m-2 p-2 text-lg border-2 h-40 '
                         'border-gray-300 '
         )
 
-        print(cls, request)
+        jp.Button(
+                    a=input_div, text="Get Definition",
+                    classes='border-2 border-black '
+                            'rounded text-gray-500 ',
+                    click=cls.get_definition,
+                    outputdiv=output_div
+        )
+        # print(cls, request)
         return wp
+
+    """
+    This time the event handler expects us to declare two params but the handler method is not
+    a function, so if we use an instance method we get self as first parameter and if we use
+    @classmethod we get the class as first parameter and in the end we always get 3 parameters
+    Either way it doesnt work so we need to use @staticmethod to treat the method as a function
+    """
+    @staticmethod
+    def get_definition(widget, msg):
+        print(widget)
+        print(msg)
+        widget.outputdiv.text = "hello"
 
 
 # jp.Route(Home.path, Home.serve)
