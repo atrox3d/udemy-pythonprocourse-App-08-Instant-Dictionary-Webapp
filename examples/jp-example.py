37579ec9c52445ffc66e3fdeebadfebf216ebeea
atrox3d <atrox3d@gmail.com>
2022-09-13 06:05:20 +0200

    reformat

--- examples/jp-example.py
+++ examples/jp-example.py
@@ -5,35 +5,67 @@ import justpy as jp
 def home():                                                     # request handler
     wp = jp.WebPage()
 
-    main_div = jp.Div(a=wp,                                     # main div, belongs to webpage
-                      classes='bg-gray-500 h-screen')           # gray, takes entire screen
+    main_div = jp.Div(                                          # main div
+                        a=wp,                                   # belongs to webpage
+                        classes='bg-gray-500 h-screen'          # gray, takes entire screen
+    )
 
-    header_div = jp.Div(a=main_div,
-                        classes='grid grid-cols-3 gap-4 '
-                                'p-4 border border-black')
-    input1 = jp.Input(a=header_div, placeholder="Enter first value",     # input, belongs to div
-             classes='form-input')
-    input2 =jp.Input(a=header_div, placeholder="Enter second value",    # input, belongs to div
-             classes='form-input')
-    result = jp.Div(a=header_div, text="Result goes here...",            # sub-div, belongs to div
-           classes='text-gray-600')
-    jp.Div(a=header_div, text="Just another div...",            # sub-div, belongs to div
-           classes='text-gray-600')
-    jp.Div(a=header_div, text="Yet another div",                # sub-div, belongs to div
-           classes='text-gray-600')
+    header_div = jp.Div(                                        # header div
+                        a=main_div,                             # belongs to main div
+                        classes='grid '                         # activate grid
+                                'grid-cols-3 '                  # a grid with 3 cols
+                                'gap-4 '                        # spacing of grid?
+                                'p-4 '                          # padding (internal space)
+                                'border '                       # activate border
+                                'border-black'                  # border color
+    )
+    input1 = jp.Input(                                          # input
+                        a=header_div,                           # belongs to header_div
+                        placeholder="Enter first value",        # text placeholder
+                        classes='form-input'
+    )
+    input2 =jp.Input(                                           # input
+                        a=header_div,                           # belongs to header div
+                        placeholder="Enter second value",
+                        classes='form-input'
+    )
+    result = jp.Div(                                            # sub-div
+                    a=header_div,                               # belongs to header div
+                    text="Result goes here...",                 # div text
+                    classes='text-gray-600'
+    )
+    jp.Div(                                                     # sub-div
+            a=header_div,                                       # belongs to header div
+            text="Just another div...",
+            classes='text-gray-600'
+    )
+    jp.Div(                                                     # sub-div
+            a=header_div,                                       # belongs to header div
+            text="Yet another div",
+            classes='text-gray-600'
+    )
 
-    footer_div = jp.Div(a=main_div, classes='grid grid-cols-2 gap-4')
-    jp.Button(a=footer_div, text='Calculate',                   # button, belongs to div
-              click=sum_up,
+    footer_div = jp.Div(                                        # footer div
+                        a=main_div,                             # belongs to main div
+                        classes='grid grid-cols-2 gap-4'        # another grid
+    )
+    jp.Button(                                                  # button
+                a=footer_div,                                   # belongs to footer div
+                text='Calculate',                               # button caption
+                click=sum_up,                                   # on click function
 
-              input1=input1, input2=input2,                     # add the two inputs as attributes
-              result=result,                                    # add the result div as attribute
+                input1=input1, input2=input2,                   # add the two inputs as attributes
+                result=result,                                  # add the result div as attribute
 
-              classes='border border-blue-500 m-2 p-2 '         # border, spacing, padding
-                      'px-4 rounded text-blue-600 '
-                      'hover:bg-red-500 '                       # mouse-over event
-                      'hover:text-white')
-    jp.Div(a=footer_div, text='I am a cool interactive div!')
+                classes='border border-blue-500 m-2 p-2 '       # border, spacing, padding
+                        'px-4 rounded text-blue-600 '             
+                        'hover:bg-red-500 '                     # mouse-over event
+                        'hover:text-white'
+    )
+    jp.Div(
+            a=footer_div,
+            text='I am a cool interactive div!'
+    )
     return wp                                                   # must return page
 
 
