5078ecb72b94a5b3e748b8a7b98594b8deb4de05
atrox3d <atrox3d@gmail.com>
2022-09-16 08:02:41 +0200

    restore imports and add comments

--- main.py
+++ main.py
@@ -3,9 +3,15 @@ import inspect
 import justpy as jp
 
 from webapp import page
-# from webapp.about import About
-# from webapp.home import Home
-# from webapp.dictionary import Dictionary
+
+"""
+imports are grayed out in pycharm but removing them will
+cause the program to fail, because they would not be into
+globals()
+"""
+from webapp.about import About
+from webapp.home import Home
+from webapp.dictionary import Dictionary
 
 # """
 # Abstract Classes: first test and error
@@ -28,11 +34,14 @@ from webapp import page
 # jp.Route(About.path, About.serve)
 # jp.Route(Dictionary.path, Dictionary .serve)
 
-imports = list(globals().values())
-for obj in imports:
-    if inspect.isclass(obj):
-        if issubclass(obj, page.Page) and obj is not page.Page:
-            jp.Route(obj.path, obj.serve)
+imports = list(globals().values())                                              # get list of values from globals (imports)
+for obj in imports:                                                             # iter through objects in globals
+    if inspect.isclass(obj):                                                    # check if obj is indeed a class
+        if (
+                issubclass(obj, page.Page) and                                  # check if we have a subclass of abstract Page
+                obj is not page.Page                                            # must not be the abstract class, if imported
+        ):
+            jp.Route(obj.path, obj.serve)                                       # automatically create routes
 
 
 jp.justpy(port=8001)
