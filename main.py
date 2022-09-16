5bdb56a72317163279e6cb500b6cf5e65db3fce4
atrox3d <atrox3d@gmail.com>
2022-09-16 06:53:31 +0200

    192. Making the Routes Automatic

--- main.py
+++ main.py
@@ -4,6 +4,24 @@ from webapp.about import About
 from webapp.home import Home
 from webapp.dictionary import Dictionary
 
+"""
+Abstract Classes: first test and error
+"""
+imports = globals()
+for key, value in imports.items():                              # RuntimeError: dictionary changed size during iteration
+    if hasattr(key, 'path'):                                    # because key and values are globals and are therefore
+        jp.Route(value.path, value.serve)                       # added to the globals dict
+
+
+"""
+Abstract Classes: second test and error
+"""
+imports = list(globals().values())
+for obj in imports:                                             # AttributeError: 'SourceFileLoader' object has no attribute 'serve'
+    if hasattr(obj, 'path'):                                    # the SourceFilellLoader object has a path attribute
+        jp.Route(obj.path, obj.serve)                           # but not a serve method
+
+
 jp.Route(Home.path, Home.serve)
 jp.Route(About.path, About.serve)
 jp.Route(Dictionary.path, Dictionary .serve)
